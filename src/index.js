09a79c4cf84ef8b991062df04722fd00c042bbf6
ajLapid718 <ajLapid718@gmail.com>
2020-03-03 10:31:17 -0500

    set up type definitions, resolvers, and apollo server

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 // set up schema, type definitions, and resolver functions here;
 // set up apollo server here;
+
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: function() {
+      return "hello world";
+    }
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// this will bootup our server on port 4000 by default;
+// to configure to a different port, pass in an options object to the listen function like below:
+// { port: 3000 };
+server.listen({ port: 3000 }).then((serverInfo) => console.log(`server started at ${serverInfo.url}`));
