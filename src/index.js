6c41cf54eb7490ba0a89e0ec3c96d6d23207fcaf
ajLapid718 <ajLapid718@gmail.com>
2020-03-03 10:32:16 -0500

    generate start script

--- src/index.js
+++ src/index.js
@@ -21,5 +21,5 @@ const server = new ApolloServer({ typeDefs, resolvers });
 
 // this will bootup our server on port 4000 by default;
 // to configure to a different port, pass in an options object to the listen function like below:
-// { port: 3000 };
-server.listen({ port: 3000 }).then((serverInfo) => console.log(`server started at ${serverInfo.url}`));
+// { port: 1234 };
+server.listen().then((serverInfo) => console.log(`server started at ${serverInfo.url}`));
